dc7a88c9477d47eace1b694e08b9331904c23cb2
TitTolkachev <tit.tolk07022003@gmail.com>
2022-03-29 00:48:03 +0700

    С горем пополам работает AlgorithmMode1 Второй режим пока не начал делать

--- js/genetic-algorithm-pro.js
+++ js/genetic-algorithm-pro.js
@@ -4,10 +4,22 @@ window.addEventListener("load", function onWindowLoad() {
     let mainButton = document.getElementById("mainButton");
     let mainText = document.getElementById("mainText");
 
-    mainButton.onclick = function () {
-            algorithm();
-    }
+    // Algorithm Modes:
+    // 1 - генерируемый код создает массив из numberOfFibonacciNumbers первых чисел Фибоначчи
+    // 2 - генерируемый код возвращает число, близкое к заданному пользователем n-ому числу Фибоначчи
+    let algorithmMode = 1;
 
+    let algorithmIsWorking = 0;
+
+    mainButton.onclick = function () {
+        if(algorithmIsWorking === 0) {
+            mainButton.textContent = "Break";
+            algorithmIsWorking = 1;
+            algorithm();
+        }
+        else
+            algorithmIsWorking = 0;
+    }
 
 
     //------------------------------------------------------------------------------------------
@@ -15,77 +27,14 @@ window.addEventListener("load", function onWindowLoad() {
     //ВАЖНЫЕ КОНСТАНТЫ ПО АЛГОРИТМУ
     const N = 200;//размер популяции
     const MutationPercent = 0.8;//процент мутаций
-    const NumberOfGenerations = 200;//количество поколений
-    //const MaxNumberOfWithoutResultGenerations = ...;
+    const NumberOfGenerations = 1000;//количество поколений
+    const MaxNumberOfWithoutResultGenerations = 100;
     const NumberOfDescendants = N;
     const ChromosomeMinLength = 1;
     const ChromosomeMaxLength = 70;
 
     //------------------------------------------------------------------------------------------
 
-
-
-    //~~~~~~~~~~~~~~~~~~~~НАБОР КОМАНД~~~~~~~~~~~~~~~~~~~~
-    let numberOfFibonacciNumbers = 10;
-
-    let functionsArray = [];
-    for(let i = 0; i < numberOfFibonacciNumbers; i++)
-    {
-        //Экзотическое:
-
-        //Разные условия:
-        functionsArray.push(`if(arr[${i}] === arr[${getRandomInt(0, numberOfFibonacciNumbers)}])\n  arr[${i}]++;\n`);
-        functionsArray.push(`if(arr[${i}] >= arr[${getRandomInt(0, numberOfFibonacciNumbers)}])\n  arr[${i}]--;\n`);
-
-        //Простые операции:
-        functionsArray.push(`arr[${i}]=arr[${getRandomInt(0, numberOfFibonacciNumbers)}];\n`);
-        functionsArray.push(`arr[${i}]+=arr[${getRandomInt(0, numberOfFibonacciNumbers)}];\n`);
-        functionsArray.push(`arr[${i}]-=arr[${getRandomInt(0, numberOfFibonacciNumbers)}];\n`);
-        functionsArray.push(`arr[${i}]++;\n`);
-        functionsArray.push(`arr[${i}]+=2;\n`);
-        functionsArray.push(`arr[${i}]+=3;\n`);
-        functionsArray.push(`arr[${i}]--;\n`);
-        functionsArray.push(`arr[${i}]-=2;\n`);
-        functionsArray.push(`arr[${i}]-=3;\n`);
-    }
-
-    let FibonacciNumbers = [];
-    for(let a = 0, b = 1, i = 0; i < numberOfFibonacciNumbers; i++, [a, b]=[b, a+b])
-        FibonacciNumbers[i] = a;
-
-
-    //нахождение приспособленности хромосомы
-    function findFitness(chromosome) {
-
-        let arr = new Array(numberOfFibonacciNumbers);
-        for(let i = 0; i < arr.length; i++)
-            arr[i] = 0;
-
-        let s = "";
-
-        for (let i = 0; i < chromosome.arr.length; i++)
-            s += functionsArray[chromosome.arr[i]];
-
-        try {
-            eval(s);
-            let sum = 0;
-            for(let i = 0; i < numberOfFibonacciNumbers; i++)
-                sum+=Math.abs(FibonacciNumbers[i]-arr[i]);
-            chromosome.fitness = sum;
-        } catch {
-            chromosome.fitness = Infinity;
-        }
-
-        s = `let numberOfFibonacciNumbers = ${numberOfFibonacciNumbers};\n` +
-            "let arr = new Array(numberOfFibonacciNumbers);\n" +
-            "for(let i = 0; i < arr.length; i++)\n" +
-            "  arr[i] = 0;\n\n" +
-            s +
-            "\nfor(let i = 0; i < arr.length; i++)\n" +
-            "  console.log(arr[i])";
-
-        return s;
-    }
 
     //случайное число
     function getRandomInt(min, max) {
@@ -95,41 +44,101 @@ window.addEventListener("load", function onWindowLoad() {
         //Максимум не включается, минимум включается
     }
 
+
+    //--------------------------------------------------------------------------------------------------------
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~АЛГОРИТМ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    //--------------------------------------------------------------------------------------------------------
+
+
     function algorithm() {
+
+
+        //----------------------------------------------------
+        //~~~~~~~~~~~~~~~~~~~~НАБОР КОМАНД~~~~~~~~~~~~~~~~~~~~
+        //----------------------------------------------------
+
+        let numberOfFibonacciNumbers = 10;
+
+        let functionsArray = [];
+        for (let i = 0; i < numberOfFibonacciNumbers; i++) {
+            //Экзотическое:
+
+            //Разные условия:
+            functionsArray.push(`if(arr[${i}] === arr[${getRandomInt(0, numberOfFibonacciNumbers)}])\n  arr[${i}]++;\n`);
+            functionsArray.push(`if(arr[${i}] >= arr[${getRandomInt(0, numberOfFibonacciNumbers)}])\n  arr[${i}]--;\n`);
+
+            //Простые операции:
+            functionsArray.push(`arr[${i}]=arr[${getRandomInt(0, numberOfFibonacciNumbers)}];\n`);
+            functionsArray.push(`arr[${i}]+=arr[${getRandomInt(0, numberOfFibonacciNumbers)}];\n`);
+            functionsArray.push(`arr[${i}]-=arr[${getRandomInt(0, numberOfFibonacciNumbers)}];\n`);
+            functionsArray.push(`arr[${i}]++;\n`);
+            functionsArray.push(`arr[${i}]+=2;\n`);
+            functionsArray.push(`arr[${i}]+=3;\n`);
+            functionsArray.push(`arr[${i}]--;\n`);
+            functionsArray.push(`arr[${i}]-=2;\n`);
+            functionsArray.push(`arr[${i}]-=3;\n`);
+        }
+
+        let FibonacciNumbers = [];
+        for (let a = 0, b = 1, i = 0; i < numberOfFibonacciNumbers; i++, [a, b] = [b, a + b])
+            FibonacciNumbers[i] = a;
+
+
+        //----------------------------------------------------
+        //~~~~~~~НАХОЖДЕНИЕ ПРИСПОСОБЛЕННОСТИ ХРОМОСОМЫ~~~~~~~
+        //----------------------------------------------------
+        function findFitness(chromosome) {
+
+            let arr = new Array(numberOfFibonacciNumbers);
+            for (let i = 0; i < arr.length; i++)
+                arr[i] = 0;
+
+            let s = "";
+
+            for (let i = 0; i < chromosome.arr.length; i++)
+                s += functionsArray[chromosome.arr[i]];
+
+            try {
+                eval(s);
+                let sum = 0;
+                for (let i = 0; i < numberOfFibonacciNumbers; i++)
+                    sum += Math.abs(FibonacciNumbers[i] - arr[i]);
+                chromosome.fitness = sum;
+            } catch {
+                chromosome.fitness = Infinity;
+            }
+
+            s = `let numberOfFibonacciNumbers = ${numberOfFibonacciNumbers};\n` +
+                "let arr = new Array(numberOfFibonacciNumbers);\n" +
+                "for(let i = 0; i < arr.length; i++)\n" +
+                "  arr[i] = 0;\n\n" +
+                s +
+                "\nfor(let i = 0; i < arr.length; i++)\n" +
+                "  console.log(arr[i])";
+
+            return s;
+        }
+
+
+        //--------------------------------------------------------------------------------------------------------
+        //НАЧАЛО АЛГОРИТМА
+        //--------------------------------------------------------------------------------------------------------
+
 
         //------------------------------------------------------------------------------------------
         //Генерация начальной популяции - создаем случайные начальные хромосомы
 
-        let chromosome = {
-            arr: new Array(getRandomInt(ChromosomeMinLength, ChromosomeMaxLength)),
-            fitness: Infinity
-        };
-        for (let i = 0; i < chromosome.arr.length; i++)
-            chromosome.arr[i] = getRandomInt(0, functionsArray.length);
-        /*
-        chromosome.arr.sort(function (a, b) {
-            return a - b
-        });
-         */
-        findFitness(chromosome);
+        let chromosome;
+        let population = [];
 
-        let population = [{
-            arr: chromosome.arr.slice(0, chromosome.arr.length),
-            fitness: chromosome.fitness
-        }];
-
-        for (let i = 1; i < N; i++) {
+        for (let i = 0; i < N; i++) {
             chromosome = {
                 arr: new Array(getRandomInt(ChromosomeMinLength, ChromosomeMaxLength)),
                 fitness: Infinity
             };
             for (let i = 0; i < chromosome.arr.length; i++)
                 chromosome.arr[i] = getRandomInt(0, functionsArray.length);
-            /*
-            chromosome.arr.sort(function (a, b) {
-                return a - b
-            });
-             */
+
             findFitness(chromosome);
 
             population.push({arr: chromosome.arr.slice(0, chromosome.arr.length), fitness: chromosome.fitness});
@@ -137,15 +146,26 @@ window.addEventListener("load", function onWindowLoad() {
         //------------------------------------------------------------------------------------------
 
 
-        //Дальше сам алгоритм
+        //------------------------------------------------------------------------------------------
+        //ДАЛЬШЕ ИТЕРАЦИИ
+        //------------------------------------------------------------------------------------------
+
         let it = 0;
+        let withoutResultIterations = 0;
         let bestFitness = Infinity;
         let bestCode = "";
 
-        while (it < NumberOfGenerations) {
+        let id = setInterval(function () {
+
+            if (it >= NumberOfGenerations || withoutResultIterations >= MaxNumberOfWithoutResultGenerations || bestFitness === 0 || !algorithmIsWorking) {
+                mainButton.textContent = "Start";
+                algorithmIsWorking = 0;
+                clearInterval(id);
+            }
+
             it++;
 
-            //создаем потомков в количестве NumberOfDescendants:
+            //Создаем потомков в количестве NumberOfDescendants:
 
             for (let createdDescendants = 0; createdDescendants < NumberOfDescendants; createdDescendants += 2) {
                 //Берем 2 случайных хромосомы(a, b) из популяции и скрещиваем их, получая потомков(descendant1,2)
@@ -153,17 +173,11 @@ window.addEventListener("load", function onWindowLoad() {
                 let v2 = getRandomInt(0, population.length);
 
 
-
-
-
                 //ВНИМАНИЕ!!!!!!!!!!!!!!!
                 //НЕ ЗАБЫТЬ ДОБАВИТЬ РАЗНЫЕ РЕЖИМЫ РАЗРЕЗА ХРОМОСОМЫ!!!!!!!!!!
 
 
-
-
-
-                //случайное место разреза хромосомы
+                //Случайное место разреза хромосомы
                 let cut1 = getRandomInt(0, Math.min(population[v1].arr.length, population[v2].arr.length));
                 let cut2 = getRandomInt(0, Math.min(population[v1].arr.length, population[v2].arr.length));
 
@@ -177,15 +191,8 @@ window.addEventListener("load", function onWindowLoad() {
                 }
 
 
-
-
-
-
                 //ВНИМАНИЕ!!!!!!!!!!!!!!!
                 //НЕ ЗАБЫТЬ ДОБАВИТЬ РАЗНЫЕ РЕЖИМЫ МУТАЦИИ!!!!!!!!!!
-
-
-
 
 
                 //------------------------------------------------------------------------------------------
@@ -221,14 +228,15 @@ window.addEventListener("load", function onWindowLoad() {
             population.sort(compare);
             population = population.slice(0, N);
 
-            //вывод нового лучшего результата
+            //Вывод нового лучшего результата
             if (population[0].fitness < bestFitness) {
-                bestFitness = population[0].fitness;
-                bestCode = findFitness(population[0]);
+                withoutResultIterations = 0;
                 mainText.textContent = it.toString();
                 mainText.textContent += ' ' + population[0].fitness;
                 mainText.textContent += '\n\n' + bestCode;
+                bestFitness = population[0].fitness;
+                bestCode = findFitness(population[0]);
             }
-        }
+        }, 0)
     }
 });
