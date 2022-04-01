3e1151578dcaf9bb87d91db88f3ad77850bd82f8
TitTolkachev <tit.tolk07022003@gmail.com>
2022-04-02 01:03:11 +0700

    Готова модель для муравьев, феромонов пока нет

--- js/ant-algorithm-pro.js
+++ js/ant-algorithm-pro.js
@@ -2,11 +2,30 @@ window.addEventListener("load", function onWindowLoad() {
 
     let MyCanvas = document.getElementById("myCanvas");
     let ctx = MyCanvas.getContext('2d');
+    let ExtraCanvas = document.getElementById("extraCanvas");
+    let extraCtx = ExtraCanvas.getContext('2d');
 
     //САМАЯ ВАЖНАЯ ВЕЩЬ!!! ПОЗВОЛЯЕТ УВЕЛИЧИТЬ ФПС!!!
     let mapPixelScale = 10;//каждый i-ый пиксель хранит объект, остальные - лишь картинка
 
+    let somethingChanged = false;//делаем перерисовку фона только тогда, когда на нем что-то поменялось
+
+    let antStepLength = 1;
+
     let mainObjects = new Array(MyCanvas.width / mapPixelScale);
+    let ants = new Array(100);
+    let anthill = {
+        isBuilt: false,
+        radius: 25,
+        x: -1,
+        y: -1,
+        color: "#ff5f6f",
+        borderColor: "#af3f4f"
+    }
+
+
+    //-------------------------------------------------------------------------
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~СТЕНЫ и ЕДА~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     for (let i = 0; i < mainObjects.length; i++) {
         mainObjects[i] = new Array(MyCanvas.height / mapPixelScale);
         for (let j = 0; j < mainObjects[i].length; j++)
@@ -20,8 +39,8 @@ window.addEventListener("load", function onWindowLoad() {
                     if (this.wall) {
 
                         //СТЕНА
-                        ctx.fillStyle = WallsColor;
-                        ctx.fillRect(this.x, this.y, mapPixelScale, mapPixelScale);
+                        extraCtx.fillStyle = "gray";
+                        extraCtx.fillRect(this.x, this.y, mapPixelScale, mapPixelScale);
                         /*ctx.strokeStyle = WallsColor;
                         ctx.fillStyle = WallsColor;
                         ctx.beginPath();
@@ -29,25 +48,44 @@ window.addEventListener("load", function onWindowLoad() {
                         ctx.closePath();
                         ctx.fill();
                         ctx.stroke();*/
-                    }else if(this.food > 0){
+                    } else if (this.food > 0) {
 
                         //ЕДА
-                        if(this.food<16){
-                            ctx.strokeStyle = `#000${(this.food).toString(16)}00`;
-                            ctx.fillStyle = `#000${(this.food).toString(16)}00`;
+                        if (this.food < 16) {
+                            extraCtx.strokeStyle = `#000${(this.food).toString(16)}00`;
+                            extraCtx.fillStyle = `#000${(this.food).toString(16)}00`;
+                        } else {
+                            extraCtx.strokeStyle = `#00${(this.food).toString(16)}00`;
+                            extraCtx.fillStyle = `#00${(this.food).toString(16)}00`;
                         }
-                        else {
-                            ctx.strokeStyle = `#00${(this.food).toString(16)}00`;
-                            ctx.fillStyle = `#00${(this.food).toString(16)}00`;
-                        }
-                        ctx.beginPath();
-                        ctx.arc(this.x+mapPixelScale/2, this.y+mapPixelScale/2, mapPixelScale/2, 0, Math.PI * 2, false);
-                        ctx.closePath();
-                        ctx.fill();
-                        ctx.stroke();
+                        extraCtx.beginPath();
+                        extraCtx.arc(this.x + mapPixelScale / 2, this.y + mapPixelScale / 2, mapPixelScale / 2, 0, Math.PI * 2, false);
+                        extraCtx.closePath();
+                        extraCtx.fill();
+                        extraCtx.stroke();
                     }
                 }
             }
+    }
+
+
+    //-------------------------------------------------------------
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~МУРАВЬИ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    function initAnts() {
+        for (let i = 0; i < ants.length; i++) {
+            let r1 = Math.random() * 2 - 1;
+            let r2 = Math.random() * 2 - 1;
+            ants[i] = {
+                Vx: r1,
+                Vy: r2,
+                x: anthill.x + (r1 * anthill.radius) / (Math.sqrt(r1 ** 2 + r2 ** 2)),
+                y: anthill.y + (r2 * anthill.radius) / (Math.sqrt(r1 ** 2 + r2 ** 2)),
+                next: function () {
+                    this.x+=(this.Vx * antStepLength) / (Math.sqrt(this.Vx ** 2 + this.Vy ** 2));
+                    this.y+=(this.Vy * antStepLength) / (Math.sqrt(this.Vx ** 2 + this.Vy ** 2));
+                }
+            }
+        }
     }
 
 
@@ -60,8 +98,6 @@ window.addEventListener("load", function onWindowLoad() {
     let brushWidth = document.getElementById("brushWidth");
 
     let drawingMode = new Array(4);//0 - еда, 1 - стены, 2 - муравьи, 3 - ластик
-
-    const WallsColor = "gray";
 
 
     //---------------------------------------------------------
@@ -80,6 +116,10 @@ window.addEventListener("load", function onWindowLoad() {
         drawingMode[1] = false;
         drawingMode[2] = false;
         drawingMode[3] = false;
+
+        anthill.isBuilt = false;
+
+        somethingChanged = true;
     }
 
     foodButton.onclick = function () {
@@ -117,14 +157,13 @@ window.addEventListener("load", function onWindowLoad() {
 
                 let width = brushWidth.value;
 
-                for(let i = Math.max(0, x-Math.floor(width/mapPixelScale)); i<Math.min(MyCanvas.width/mapPixelScale, x+Math.floor(width/mapPixelScale)); i++)
-                {
-                    for(let j = Math.max(0, y-Math.floor(width/mapPixelScale)); j<Math.min(MyCanvas.height/mapPixelScale, y+Math.floor(width/mapPixelScale)); j++)
-                    {
-                        mainObjects[i][j].food = Math.min(255, mainObjects[i][j].food+6);
+                for (let i = Math.max(0, x - Math.floor(width / mapPixelScale)); i < Math.min(MyCanvas.width / mapPixelScale, x + Math.floor(width / mapPixelScale)); i++) {
+                    for (let j = Math.max(0, y - Math.floor(width / mapPixelScale)); j < Math.min(MyCanvas.height / mapPixelScale, y + Math.floor(width / mapPixelScale)); j++) {
+                        mainObjects[i][j].food = Math.min(255, mainObjects[i][j].food + 6);
                         mainObjects[i][j].notEmpty = true;
                     }
                 }
+                somethingChanged = true;
 
             } else if (drawingMode[1]) {
 
@@ -134,10 +173,8 @@ window.addEventListener("load", function onWindowLoad() {
 
                 let width = brushWidth.value;
 
-                for(let i = Math.max(0, x-Math.floor(width/mapPixelScale)); i<Math.min(MyCanvas.width/mapPixelScale, x+Math.floor(width/mapPixelScale)); i++)
-                {
-                    for(let j = Math.max(0, y-Math.floor(width/mapPixelScale)); j<Math.min(MyCanvas.height/mapPixelScale, y+Math.floor(width/mapPixelScale)); j++)
-                    {
+                for (let i = Math.max(0, x - Math.floor(width / mapPixelScale)); i < Math.min(MyCanvas.width / mapPixelScale, x + Math.floor(width / mapPixelScale)); i++) {
+                    for (let j = Math.max(0, y - Math.floor(width / mapPixelScale)); j < Math.min(MyCanvas.height / mapPixelScale, y + Math.floor(width / mapPixelScale)); j++) {
                         mainObjects[i][j].wall = true;
                         mainObjects[i][j].food = 0;
                         mainObjects[i][j].toFoodPheromones = 0;
@@ -145,10 +182,7 @@ window.addEventListener("load", function onWindowLoad() {
                         mainObjects[i][j].notEmpty = true;
                     }
                 }
-
-            } else if (drawingMode[2]) {
-
-                //СПАВН КОЛОНИИ!!!
+                somethingChanged = true;
 
             } else if (drawingMode[3]) {
 
@@ -158,28 +192,88 @@ window.addEventListener("load", function onWindowLoad() {
 
                 let width = brushWidth.value;
 
-                for(let i = Math.max(0, x-Math.floor(width/mapPixelScale)); i<Math.min(MyCanvas.width/mapPixelScale, x+Math.floor(width/mapPixelScale)); i++)
-                {
-                    for(let j = Math.max(0, y-Math.floor(width/mapPixelScale)); j<Math.min(MyCanvas.height/mapPixelScale, y+Math.floor(width/mapPixelScale)); j++)
-                    {
+                for (let i = Math.max(0, x - Math.floor(width / mapPixelScale)); i < Math.min(MyCanvas.width / mapPixelScale, x + Math.floor(width / mapPixelScale)); i++) {
+                    for (let j = Math.max(0, y - Math.floor(width / mapPixelScale)); j < Math.min(MyCanvas.height / mapPixelScale, y + Math.floor(width / mapPixelScale)); j++) {
                         mainObjects[i][j].notEmpty = false;
                         mainObjects[i][j].wall = false;
                         mainObjects[i][j].food = 0;
                     }
                 }
+                somethingChanged = true;
             }
         }
     };
 
 
-    //----------------------------------------------------
-    //----------------------------------------------------
-    //~~~~~~~~~~~~~~~ОТРИСОВКА И ПЕРЕРАСЧЕТ~~~~~~~~~~~~~~~
-    setInterval(function () {
-        ctx.clearRect(0, 0, MyCanvas.width, MyCanvas.height);
+    MyCanvas.onmousedown = function (e) {
+        if (drawingMode[2]) {
+
+            //СПАВН КОЛОНИИ!!!
+            if (!anthill.isBuilt && e.offsetX > anthill.radius * 2 && e.offsetY > anthill.radius * 2 && e.offsetX < MyCanvas.width - anthill.radius * 2 && e.offsetY < MyCanvas.height - anthill.radius * 2) {
+                anthill.isBuilt = true;
+                anthill.x = e.offsetX;
+                anthill.y = e.offsetY;
+
+                initAnts();
+                somethingChanged = true;
+            }
+        }
+    }
+
+
+    function drawAnts() {
+        for (let i = 0; i < ants.length; i++) {
+            ctx.beginPath();
+            ctx.fillStyle = "red";
+            ctx.strokeStyle = "black";
+            ctx.arc(ants[i].x, ants[i].y, 2, 0, Math.PI * 2, false);
+            ctx.closePath();
+            ctx.fill();
+            ctx.stroke();
+        }
+    }
+
+
+    //---------------------------------
+    //---------------------------------
+    //ОТРИСОВКА ВСПОМОГАТЕЛЬНОГО CANVAS
+    function updateExtraCtx() {
+        extraCtx.clearRect(0, 0, MyCanvas.width, MyCanvas.height);
         for (let i = 0; i < mainObjects.length; i++)
             for (let j = 0; j < mainObjects[i].length; j++)
                 if (mainObjects[i][j].notEmpty)
                     mainObjects[i][j].next();
+
+        if (anthill.isBuilt) {
+            extraCtx.beginPath();
+            extraCtx.fillStyle = anthill.color;
+            extraCtx.strokeStyle = anthill.borderColor;
+            extraCtx.arc(anthill.x, anthill.y, anthill.radius, 0, Math.PI * 2, false);
+            extraCtx.closePath();
+            extraCtx.fill();
+            extraCtx.stroke();
+        }
+    }
+
+    //----------------------------------------------------
+    //----------------------------------------------------
+    //~~~~~~~~~~~~~~~ОТРИСОВКА И ПЕРЕРАСЧЕТ~~~~~~~~~~~~~~~
+    setInterval(function () {
+
+        //перерисовка фона, если на нем что-то поменялось
+        if (somethingChanged)
+            updateExtraCtx();
+
+        somethingChanged = false;
+
+        ctx.clearRect(0, 0, MyCanvas.width, MyCanvas.height);
+        ctx.drawImage(ExtraCanvas, 0, 0);
+
+        if (anthill.isBuilt) {
+            for (let i = 0; i < ants.length; i++)
+                ants[i].next();
+            drawAnts();
+        }
+
     }, 0);
 });
