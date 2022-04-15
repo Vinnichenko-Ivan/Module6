6ff74baf4d5ffbca7b3ebc60b17b3efa97b01de9
cyberian <fox4080@gmail.com>
2022-04-15 23:18:42 +0700

    Добавлены статьи на которые опирался.

--- js/pro/cluster/clusterAMulti.js
+++ js/pro/cluster/clusterAMulti.js
@@ -1,3 +1,9 @@
+//https://habr.com/ru/post/101338/
+//https://ru.wikipedia.org/wiki/%D0%9A%D0%BB%D0%B0%D1%81%D1%82%D0%B5%D1%80%D0%BD%D1%8B%D0%B9_%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D0%B7
+//https://neerc.ifmo.ru/wiki/index.php?title=%D0%9A%D0%BB%D0%B0%D1%81%D1%82%D0%B5%D1%80%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F
+//https://habr.com/ru/post/67078/
+//https://ru.wikipedia.org/wiki/DBSCAN
+
 let drawCenters = true;
 let drawLines = true;
 let autoRun = true;
