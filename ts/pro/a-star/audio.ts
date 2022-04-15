4817e0a42dfa1808c1bf676a95f8bbaa45833520
GreenpixDev <kol-roman.green@mail.ru>
2022-04-15 20:36:41 +0700

    Исправления
    - Добавил jquery в проект
    - Сделал конечный автомат состояний для кнопки музыки
    - Рефакторинг

---
+++ ts/pro/a-star/audio.ts
@@ -0,0 +1,30 @@
+// Название кнопок для различных состояний
+const displayOn = 'Добавить атмосферы';
+const displayOff = 'Сделать скучным =|';
+
+// Громкость музыки
+const volume = 0.05;
+
+// Музыка
+let audio: HTMLAudioElement;
+
+function playAudio(button: HTMLElement) {
+    if (audio != undefined && !audio.paused) {
+        audio.pause();
+        audio.currentTime = 0;
+        return;
+    }
+
+    let audioId = button.getAttribute('target');
+    audio = <HTMLAudioElement> $(`audio#${audioId}`)[0];
+    audio.volume = volume;
+    audio.onpause = () => button.textContent = displayOn;
+
+    audio.play().then();
+
+    button.textContent = displayOff;
+}
+
+export function initializeAudio() {
+    $('.audio').on('click', e => playAudio(e.target));
+}
